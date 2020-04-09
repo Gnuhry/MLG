c5731158793df76f047d62d11a2b569c230cff45
Jannik Junker <geisterhauserde@gmail.com>
2020-04-09 16:55:06 +0200

    added 3rd block

---
+++ src/main/java/net/mcreator/pmtinfai/block/Cross2Block.java
@@ -0,0 +1,49 @@
+
+package net.mcreator.pmtinfai.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.pmtinfai.PMTINFAIElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@PMTINFAIElements.ModElement.Tag
+public class Cross2Block extends PMTINFAIElements.ModElement {
+	@ObjectHolder("pmtinfai:cross2")
+	public static final Block block = null;
+	public Cross2Block(PMTINFAIElements instance) {
+		super(instance, 2);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.IRON).sound(SoundType.STONE).hardnessAndResistance(1f, 10f).lightValue(0));
+			setRegistryName("cross2");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
