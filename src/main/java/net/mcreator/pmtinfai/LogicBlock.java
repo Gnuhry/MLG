6b3161c8cf4f34a9416c2cd951ce92bfd99412ac
Gnuhry <truongxh1@gmail.com>
2020-04-17 12:14:30 +0200

    Add Logic Class (clean)

--- src/main/java/net/mcreator/pmtinfai/LogicBlock.java
+++ src/main/java/net/mcreator/pmtinfai/LogicBlock.java
@@ -1,33 +1,32 @@
 
 package net.mcreator.pmtinfai;
 
-import net.minecraft.state.DirectionProperty;
-import net.minecraft.block.HorizontalBlock;
-import net.minecraft.state.properties.BlockStateProperties;
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.block.SoundType;
-import net.minecraft.state.IntegerProperty;
-import net.minecraft.state.StateContainer;
-import net.minecraft.block.BlockState;
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.world.World;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.Rotation;
 import net.minecraft.util.Mirror;
-import net.minecraft.item.BlockItemUseContext;
-import net.minecraft.world.IBlockReader;
 import net.minecraft.util.Direction;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.Blocks;
-import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.IntegerProperty;
+import net.minecraft.state.DirectionProperty;
 import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.HorizontalBlock;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
 import java.util.List;
 import java.util.Collections;
 
 public abstract class LogicBlock extends Block {
 	public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
 	public static final IntegerProperty POWER = BlockStateProperties.POWER_0_15;
-
 	public LogicBlock() {
 		super(Block.Properties.create(Material.REDSTONE_LIGHT).sound(SoundType.STEM).hardnessAndResistance(1f, 10f).lightValue(0));
 		this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH).with(POWER, Integer.valueOf(0)));
@@ -76,43 +75,31 @@ public abstract class LogicBlock extends Block {
 	}
 
 	@Override
+	public int getStrongPower(BlockState blockState, IBlockReader blockAccess, BlockPos pos, Direction side) {
+		return blockState.get(FACING) == side ? blockState.get(POWER) : 0;
+	}
+
+	@Override
 	public void neighborChanged(BlockState state, World world, BlockPos pos, Block neighborBlock, BlockPos fromPos, boolean moving) {
 		super.neighborChanged(state, world, pos, neighborBlock, fromPos, moving);
-		if (!neighborBlock.canProvidePower(state))
-			return;
-
-		BlockPos input1_pos = pos.offset(state.get(FACING).rotateY());
-		BlockPos input2_pos = pos.offset(state.get(FACING).rotateYCCW());
-		
-		int input1_value = IsRedstonePowered(world.getBlockState(input1_pos));
-		int input2_value = IsRedstonePowered(world.getBlockState(input2_pos));
-		int help=logic(input1_value, input2_value);
-		world.setBlockState(pos, world.getBlockState(pos).with(POWER, logic(input1_value, input2_value)), 2);
-		//world.notifyNeighborsOfStateExcept(pos, state.getBlock(), state.get(FACING).getOpposite());
-		//world.neighborChanged(pos, state.getBlock(), fromPos);
-		System.out.println("----------------------------"+input1_value+", "+input2_value+" = "+state.get(POWER)+", "+help);
-		//TODO:O UPDATE BLOCKSTATE
-		
-		//world.neighborChanged(pos.offset(state.get(FACING).getOpposite()), world.getBlockState((pos.offset(state.get(FACING).getOpposite()))).getBlock(), pos);
-		/*if (state.get(POWER) > 0) {
-			world.setBlockState(pos, world.getBlockState(pos).with(POWERED, Boolean.getBoolean("true")), 2);
-		} else {
-			world.setBlockState(pos, world.getBlockState(pos).with(POWERED, Boolean.getBoolean("false")), 2);
-		}*/
-
+		world.setBlockState(pos,
+				world.getBlockState(pos).with(POWER, logic(IsRedstonePowered(world.getBlockState(pos.offset(state.get(FACING).rotateY()))),
+						IsRedstonePowered(world.getBlockState(pos.offset(state.get(FACING).rotateYCCW()))))),
+				2);
+		// TODO:O UPDATE BLOCKSTATE
 	}
-	public int IsRedstonePowered(BlockState bs){
-		if(bs.has(BlockStateProperties.POWER_0_15))
-			return bs.get(BlockStateProperties.POWER_0_15)-1;
-		if(bs.has(BlockStateProperties.POWERED) && bs.get(BlockStateProperties.POWERED))
+
+	public int IsRedstonePowered(BlockState bs) {
+		if (bs.has(BlockStateProperties.POWER_0_15))
+			return bs.get(BlockStateProperties.POWER_0_15) - 1;
+		if (bs.has(BlockStateProperties.POWERED) && bs.get(BlockStateProperties.POWERED))
 			return 15;
-		if(bs.has(BlockStateProperties.LIT) && bs.get(BlockStateProperties.LIT))
+		if (bs.has(BlockStateProperties.LIT) && bs.get(BlockStateProperties.LIT))
 			return 15;
-		if(bs.getBlock()==Blocks.REDSTONE_BLOCK)
+		if (bs.getBlock() == Blocks.REDSTONE_BLOCK)
 			return 15;
 		return 0;
 	}
 
 	public abstract int logic(int first_value, int second_value);
-
 }
--- src/main/java/net/mcreator/pmtinfai/block/TestInterfaceBlock.java
+++ src/main/java/net/mcreator/pmtinfai/block/TestInterfaceBlock.java
@@ -3,28 +3,13 @@ package net.mcreator.pmtinfai.block;
 
 import net.minecraftforge.registries.ObjectHolder;
 
-import net.minecraft.world.storage.loot.LootContext;
-import net.minecraft.util.Rotation;
-import net.minecraft.util.Mirror;
-import net.minecraft.util.Direction;
-import net.minecraft.state.StateContainer;
-import net.minecraft.state.DirectionProperty;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
-import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.BlockItem;
-import net.minecraft.block.material.Material;
-import net.minecraft.block.SoundType;
-import net.minecraft.block.HorizontalBlock;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.Block;
-import net.mcreator.pmtinfai.LogicBlock;
 
 import net.mcreator.pmtinfai.PMTINFAIElements;
-
-import java.util.List;
-import java.util.Collections;
+import net.mcreator.pmtinfai.LogicBlock;
 
 @PMTINFAIElements.ModElement.Tag
 public class TestInterfaceBlock extends PMTINFAIElements.ModElement {
@@ -40,16 +25,16 @@ public class TestInterfaceBlock extends PMTINFAIElements.ModElement {
 		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.REDSTONE)).setRegistryName(block.getRegistryName()));
 	}
 	public static class CustomBlock extends LogicBlock {
-		public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
 		public CustomBlock() {
 			setRegistryName("testinterface");
 		}
-	public int logic(int first_value, int second_value){
-		if(first_value<=0||second_value<=0)
-			return 0;
-		if(first_value<second_value)
-			return second_value;
-		return first_value;
-	}
+
+		public int logic(int first_value, int second_value) {
+			if (first_value <= 0 || second_value <= 0)
+				return 0;
+			if (first_value < second_value)
+				return second_value;
+			return first_value;
+		}
 	}
 }
