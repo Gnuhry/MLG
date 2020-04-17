e0fc6a3ebbb674cf9e950d7d14944a3339d32308
Gnuhry <truongxh1@gmail.com>
2020-04-17 12:38:08 +0200

    Add Bugfix

--- src/main/java/net/mcreator/pmtinfai/LogicBlock.java
+++ src/main/java/net/mcreator/pmtinfai/LogicBlock.java
@@ -3,6 +3,7 @@ package net.mcreator.pmtinfai;
 
 import net.minecraft.world.storage.loot.LootContext;
 import net.minecraft.world.World;
+import net.minecraft.world.IWorldReader;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.Rotation;
@@ -16,6 +17,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.RedstoneWireBlock;
 import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.BlockState;
@@ -82,23 +84,40 @@ public abstract class LogicBlock extends Block {
 	@Override
 	public void neighborChanged(BlockState state, World world, BlockPos pos, Block neighborBlock, BlockPos fromPos, boolean moving) {
 		super.neighborChanged(state, world, pos, neighborBlock, fromPos, moving);
-		world.setBlockState(pos,
-				world.getBlockState(pos).with(POWER, logic(IsRedstonePowered(world.getBlockState(pos.offset(state.get(FACING).rotateY()))),
-						IsRedstonePowered(world.getBlockState(pos.offset(state.get(FACING).rotateYCCW()))))),
-				2);
-		// TODO:O UPDATE BLOCKSTATE
+		world.setBlockState(pos, world.getBlockState(pos).with(POWER, getPowerOnSides(world, pos, state)), 2);
+		Direction direction = state.get(FACING);
+		BlockPos blockpos = pos.offset(direction.getOpposite());
+		if (net.minecraftforge.event.ForgeEventFactory
+				.onNeighborNotify(world, pos, world.getBlockState(pos), java.util.EnumSet.of(direction.getOpposite()), false).isCanceled())
+			return;
+		world.neighborChanged(blockpos, this, pos);
+		world.notifyNeighborsOfStateExcept(blockpos, this, direction);
 	}
 
-	public int IsRedstonePowered(BlockState bs) {
-		if (bs.has(BlockStateProperties.POWER_0_15))
-			return bs.get(BlockStateProperties.POWER_0_15) - 1;
-		if (bs.has(BlockStateProperties.POWERED) && bs.get(BlockStateProperties.POWERED))
-			return 15;
-		if (bs.has(BlockStateProperties.LIT) && bs.get(BlockStateProperties.LIT))
-			return 15;
-		if (bs.getBlock() == Blocks.REDSTONE_BLOCK)
-			return 15;
-		return 0;
+	protected int getPowerOnSides(IWorldReader worldIn, BlockPos pos, BlockState state) {
+		Direction direction = state.get(FACING);
+		Direction direction1 = direction.rotateY();
+		Direction direction2 = direction.rotateYCCW();
+		return logic(this.getPowerOnSide(worldIn, pos.offset(direction1), direction1),
+				this.getPowerOnSide(worldIn, pos.offset(direction2), direction2));
+	}
+
+	protected int getPowerOnSide(IWorldReader worldIn, BlockPos pos, Direction side) {
+		BlockState blockstate = worldIn.getBlockState(pos);
+		Block block = blockstate.getBlock();
+		if (this.isAlternateInput(blockstate)) {
+			if (block == Blocks.REDSTONE_BLOCK) {
+				return 15;
+			} else {
+				return block == Blocks.REDSTONE_WIRE ? blockstate.get(RedstoneWireBlock.POWER) : worldIn.getStrongPower(pos, side);
+			}
+		} else {
+			return 0;
+		}
+	}
+
+	protected boolean isAlternateInput(BlockState state) {
+		return state.canProvidePower();
 	}
 
 	public abstract int logic(int first_value, int second_value);
