28b51cf831401f24cd037264ae6b8a74c530a292
Gnuhry <truongxh1@gmail.com>
2020-04-18 14:30:48 +0200

    add Creative Tab

--- src/main/java/net/mcreator/pmtinfai/block/AndGateBlockBlock.java
+++ src/main/java/net/mcreator/pmtinfai/block/AndGateBlockBlock.java
@@ -3,34 +3,34 @@ package net.mcreator.pmtinfai.block;
 
 import net.minecraftforge.registries.ObjectHolder;
 
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.item.BlockItem;
 import net.minecraft.block.Block;
 
+import net.mcreator.pmtinfai.itemgroup.LogicBlocksItemGroup;
 import net.mcreator.pmtinfai.PMTINFAIElements;
-import net.mcreator.pmtinfai.LogicBlock;
 
 @PMTINFAIElements.ModElement.Tag
 public class AndGateBlockBlock extends PMTINFAIElements.ModElement {
 	@ObjectHolder("pmtinfai:andgateblock")
 	public static final Block block = null;
 	public AndGateBlockBlock(PMTINFAIElements instance) {
-		super(instance, 1);
+		super(instance, 3);
 	}
 
 	@Override
 	public void initElements() {
 		elements.blocks.add(() -> new CustomBlock());
-		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.REDSTONE)).setRegistryName(block.getRegistryName()));
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(LogicBlocksItemGroup.tab)).setRegistryName(block.getRegistryName()));
 	}
-	public static class CustomBlock extends LogicBlock {
+	public static class CustomBlock extends net.mcreator.pmtinfai.LogicBlock {
 		public CustomBlock() {
 			setRegistryName("andgateblock");
 		}
 
-		protected int logic(int first_value, int second_value){
-			return first_value==0||second_value==0 ? 0 : first_value < second_value ? second_value : first_value;
+		protected int logic(int first_value, int second_value) {
+			return first_value == 0 || second_value == 0 ? 0 : first_value < second_value ? second_value : first_value;
 		}
 	}
 }
---
+++ src/main/java/net/mcreator/pmtinfai/itemgroup/LogicBlocksItemGroup.java
@@ -0,0 +1,35 @@
+
+package net.mcreator.pmtinfai.itemgroup;
+
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+
+import net.mcreator.pmtinfai.block.AndGateBlockBlock;
+import net.mcreator.pmtinfai.PMTINFAIElements;
+
+@PMTINFAIElements.ModElement.Tag
+public class LogicBlocksItemGroup extends PMTINFAIElements.ModElement {
+	public LogicBlocksItemGroup(PMTINFAIElements instance) {
+		super(instance, 2);
+	}
+
+	@Override
+	public void initElements() {
+		tab = new ItemGroup("tablogicblocks") {
+			@OnlyIn(Dist.CLIENT)
+			@Override
+			public ItemStack createIcon() {
+				return new ItemStack(AndGateBlockBlock.block, (int) (1));
+			}
+
+			@OnlyIn(Dist.CLIENT)
+			public boolean hasSearchBar() {
+				return false;
+			}
+		};
+	}
+	public static ItemGroup tab;
+}
